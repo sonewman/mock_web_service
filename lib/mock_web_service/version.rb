f0120c504b14d6bee775660e798645d77712110f
Sam Newman <newmansam@outlook.com>
2015-04-01 12:42:55 +0100

    v2.0.0

--- lib/mock_web_service/version.rb
+++ lib/mock_web_service/version.rb
@@ -1,3 +1,7 @@
+# update the version number in `version.rb`, and then run
+# `bundle exec rake release` to create a git tag for the
+# version, push git commits and tags, and push
+
 module MockWebService
-  VERSION = "1.0.1"
+  VERSION = "2.0.0"
 end
